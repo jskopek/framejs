4e6828ed5c325318712d16f892a4a21e89183bb1
Jean-Marc Skopek <jskopek@gmail.com>
2017-08-11 23:51:38 -0700

    Added dynamic image compilation via Jimp

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const fs = require('fs');
 const path = require('path');
+const Jimp = require('jimp');
 
 var imageServer = function(options) {
     let imageSourcePath = options.imageSourcePath;
     let imageDestPath = options.imageDestPath;
-    console.log('new ImageServer');
 
     return function(req, res, next) {
-        var imageName = req.url.split('/').pop(); // get the full url (e.g. /1.jpg) and pop the last component
+        var options = {
+            'quality': parseInt(req.query['quality']) || 100,
+            'width': parseInt(req.query['width']) || Jimp.AUTO,
+            'height': parseInt(req.query['height']) || Jimp.AUTO
+        };
+
+        var imageName = req.path.split('/').pop(); // get the full url (e.g. /1.jpg) and pop the last component
         var imagePath = path.join(imageSourcePath, imageName);
-        fs.readFile(imagePath, function(err, image) {
-            res.writeHead(200, {'Content-Type': 'image/jpg'});
-            res.end(image, 'binary');
-            next();
+
+        Jimp.read(imagePath, function(err, image) {
+            if(err) throw err;
+            image
+                .quality(options.quality)
+                .resize(options.width, options.height)
+                .getBuffer(Jimp.MIME_JPEG, function(err, image) {
+                    res.writeHead(200, {'Content-Type': 'image/jpg'});
+                    res.end(image, 'binary');
+                    next();
+                });
         });
     }
 }
